876cd136a563bf053383ed3688ed45ad0acc90fb
asmaa-ezz <you@example.com>
2018-08-15 13:30:21 +0300

    Add Sign up page

--- src/router.js
+++ src/router.js
@@ -1,19 +1,29 @@
 //const handleSignUpPage = require('./handler/');
 const addPost = require('./handler/add_post');
 
+const {handleAddUser,handlerSignupPage} = require('./handler/handleSignUpPage');
+
 const { handleOtherFiles, handlePageNotFound, handleHomePage } = require('./handler/functions');
 
 const router = (request, response) => {
     const endpoint = request.url;
-    console.log(request.method);
+    const method = request.method;
+
     if (endpoint === '/home') {
         handleHomePage(request, response);
     } else if (endpoint.includes('public')) {
         handleOtherFiles(request, response);
     }
-    else if (endpoint === '/signup') {
-        handleSignUpPage(request, response);
-    } else if (endpoint === '/add_post' && request.method==='POST') {
+    else if(endpoint === '/sign_up' && method === 'POST'){
+        handleAddUser(request,response);
+    }
+    else if(endpoint ==='/sign_up' && method ==='GET'){
+        handlerSignupPage(request,response);
+    }
+    else if (endpoint === '/sign_in' && method ==='GET') {
+        // addPost(request, response);
+    }
+    else if (endpoint === '/add_post' && method ==='POST') {
         addPost(request, response);
     } else {
         handlePageNotFound(request, response);
