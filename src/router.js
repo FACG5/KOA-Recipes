1f75da32fb4b59a82a6f4c7be95a28098ada0acc
knanahassouna1 <knanahassouna2@gmail.com>
2018-08-15 13:52:37 +0300

    Edit Home Page

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 //const handleSignUpPage = require('./handler/');
 const addPost = require('./handler/add_post');
-
-const {handleAddUser,handlerSignupPage} = require('./handler/handleSignUpPage');
+const getRecipes = require('./handler/getRecipes');
+const { handleAddUser, handlerSignupPage } = require('./handler/handleSignUpPage');
 
 const { handleOtherFiles, handlePageNotFound, handleHomePage } = require('./handler/functions');
 
 const router = (request, response) => {
     const endpoint = request.url;
 
-    
+
     const method = request.method;
 
     if (endpoint === '/home') {
@@ -16,16 +16,17 @@ const router = (request, response) => {
     } else if (endpoint.includes('public')) {
         handleOtherFiles(request, response);
     }
-    else if(endpoint === '/sign_up' && method === 'POST'){
-        handleAddUser(request,response);
+    else if (endpoint === '/sign_up' && method === 'POST') {
+        handleAddUser(request, response);
     }
-    else if(endpoint ==='/sign_up' && method ==='GET'){
-        handlerSignupPage(request,response);
+    else if (endpoint === '/sign_up' && method === 'GET') {
+        handlerSignupPage(request, response);
     }
-    else if (endpoint === '/sign_in' && method ==='GET') {
-        // addPost(request, response);
+    else if (endpoint === '/add_post' && request.method === 'GET') {
+        getRecipes(request, response);
+
     }
-    else if (endpoint === '/add_post' && method ==='POST') {
+    else if (endpoint === '/add_post' && method === 'POST') {
         addPost(request, response);
     } else {
         handlePageNotFound(request, response);
